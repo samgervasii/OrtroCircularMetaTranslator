b102ddac966fb468fe4868866bc8c9eb807da9e1
Samuele Gervasi <gervasi.samuele@educ.di.unito.it>
2021-11-22 19:13:54 +0100

    Visitor OrtroPythonTranslator setted for a base implementation

---
+++ (Visitor)OrtrOPythonTranslator/IO/output.py
@@ -0,0 +1,16 @@
+def ciao_fwd ( x , y ) : 
+    x += 4 
+    y -= 10 
+    return x , y 
+def ciao_bwd ( x , y ) : 
+    y += 10 
+    x -= 4 
+    return x , y   
+def main ( ) : 
+    x = 3 
+    y = 5 
+    f = ciao_fwd ( x , y ) 
+    print ( f ) 
+    b = ciao_bwd ( f [ 0 ] , f [ 1 ] ) 
+    print ( b ) 
+main ( ) 
